f5b7444ee5bef920eae9bc2140afc3fe8ea07c5a
SeeChen Ng <seechen81@gmail.com>
2013-04-14 17:36:44 +0800

    lab 2: exercise 5: init the kernel Address Space
    1. mem_init
    2. revise boot_map_region
    3. revise page_insert

--- kern/pmap.c
+++ kern/pmap.c
@@ -171,6 +171,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, UPAGES, (UVPT - UPAGES), PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -183,6 +184,8 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, (KSTACKTOP - KSTKSIZE), KSTKSIZE, 
+			PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -192,6 +195,7 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, KERNBASE, (0xffffffff - KERNBASE), 0, PTE_W);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
@@ -403,7 +407,8 @@ boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm
 
 	for (i = 0; i < size; i += PGSIZE) {
 		ppte = pgdir_walk(pgdir, (void *)(va + i), true);
-		*ppte |= (PTE_ADDR(pa + i) | perm | PTE_P);
+		pgdir[PDX(va + i)] = (PTE_ADDR(pgdir[PDX(va + i)]) | perm | PTE_P);
+		*ppte = (PTE_ADDR(pa + i) | perm | PTE_P);
 	}
 }
 
@@ -448,7 +453,7 @@ page_insert(pde_t *pgdir, struct PageInfo *pp, void *va, int perm)
 	if (!(*pte & PTE_P)) 
 		pp->pp_ref++;
 	*pte = (page2pa(pp) | perm | PTE_P);
-	pgdir[PDX(va)] |= (perm | PTE_P);
+	pgdir[PDX(va)] = (PTE_ADDR(pgdir[PDX(va)]) | (perm | PTE_P));
 
 	return 0;
 }
